ea50f8bb2ff4c174b245da9b7ba8476fcc8acb59
Dalton <daltonstwilliams@gmail.com>
2023-12-27 21:11:45 -0600

    Updated README. Fixed crash with printing abilities if API couldn't load anything

--- Resources/Ability.py
+++ Resources/Ability.py
@@ -12,13 +12,30 @@ class Ability(AbstractData):
         self.fromMainSeries = data.get('is_main_series')
         self.firstGeneration = data.get('generation')
         effects = data.get('effect_entries')
+        self.shortEffect = None
+        self.flavorText = None
         for effect in effects:
             if effect.get('language').get('name') != 'en':
                 continue
-            self.description = effect.get('short_effect')
+            self.shortEffect = effect.get('short_effect')
+            break
+
+        flavorTexts = data.get('flavor_text_entries')
+        for text in flavorTexts:
+            if text.get('language').get('name') != 'en':
+                continue
+            self.flavorText = text.get('flavor_text')
             break
 
         self.ID_TO_NAME_CACHE[self.ID] = self.name
+
+    @property
+    def PrintDescription(self):
+        if self.shortEffect is not None:
+            return self.shortEffect
+        if self.flavorText is not None:
+            return self.flavorText
+        return "No description available"
 
     def PrintData(self):
         return
