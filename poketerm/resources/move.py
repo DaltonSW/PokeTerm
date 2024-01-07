8001e04dada6862e13f383c08e7cb2eb4b95b6b7
Will <willwolf3@gmail.com>
2024-01-07 15:46:18 -0500

    Added effect chance
    - changed effect to short effect
    - added TM prefix and boldened
    - added effect chance to description

--- poketerm/resources/move.py
+++ poketerm/resources/move.py
@@ -1,4 +1,5 @@
 from rich.table import Table
+import re
 
 from poketerm.resources.data import AbstractData
 from poketerm.console import console
@@ -23,15 +24,15 @@ class Move(AbstractData):
         self.power: int = data.get("power")
         self.moveClass: str = data.get("damage_class").get("name")
         self.type: str = data.get("type").get("name")
-        self.effectEntry: str = data.get("effect_entries")[0].get("short_effect")
+        self.effectEntry: str = data.get("effect_entries")[0].get("effect")
 
     def PrintData(self):
         console.clear()
 
-        console.print(f"[bold]Move:[/] {self.PrintName} [{self.ID}]")
+        console.print(f"[bold]Move:[/] {self.PrintName} [[bold]TM{self.ID}[/]]")
         console.print(f"[bold]Type:[/] {self.FormattedMoveType}")
         console.print(f"[bold]Class:[/] {self.FormattedMoveClass}")
-        console.print(f"[bold]Description:[/] {self.effectEntry}")
+        console.print(f"[bold]Description:[/] {self.FormattedEffectEntry}")
 
         statTable = Table()
         statTable.add_column("PP")
@@ -52,5 +53,9 @@ class Move(AbstractData):
     @property
     def FormattedMoveType(self) -> str:
         return f"[{self.type}]{self.type.title()}[/]"
+    
+    @property
+    def FormattedEffectEntry(self) -> str:
+        return f"{re.sub("\$effect_chance%", str(self.effectChance) + "%", self.effectEntry)}"
 
     # endregion
