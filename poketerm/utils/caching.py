191d97a5631cfff3d7d8613c270f19a15b9451d2
Dalton <daltonstwilliams@gmail.com>
2024-02-04 09:53:57 -0600

    Renamed caching functions

--- poketerm/utils/caching.py
+++ poketerm/utils/caching.py
@@ -44,38 +44,39 @@ class CacheManager:
         return subcache.ID_TO_DATA.get(ID, None)
 
 
-def CacheDirectory():
+def get_cache_dir():
     return os.path.join(os.path.expanduser("~"), os.sep, ".poketerm")
 
 
-def CacheFilePath(cacheType) -> str:
-    return os.path.join(CacheDirectory(), f"{cacheType}.cache")
+def get_cache_filepath(cacheType) -> str:
+    return os.path.join(get_cache_dir(), f"{cacheType}.cache")
 
 
-def VerifyCacheDir():
-    if not os.path.exists(CacheDirectory()):
-        os.makedirs(CacheDirectory())
+# Checks if the cache directory exists. Creates it if it doesn't
+def verify_cache_dir():
+    if not os.path.exists(get_cache_dir()):
+        os.makedirs(get_cache_dir())
 
 
-def RemoveCacheDir():
-    if os.path.exists(CacheDirectory()):
-        shutil.rmtree(CacheDirectory())
+def remove_cache_dir():
+    if os.path.exists(get_cache_dir()):
+        shutil.rmtree(get_cache_dir())
 
 
-def CacheExists(cacheType) -> bool:
-    return os.path.exists(CacheFilePath(cacheType))
+def does_cache_type_exist(cacheType) -> bool:
+    return os.path.exists(get_cache_filepath(cacheType))
 
 
-def SaveCache(cacheType, cache) -> None:
-    with open(CacheFilePath(cacheType), "wb") as f:
+def save_cache(cacheType, cache) -> None:
+    with open(get_cache_filepath(cacheType), "wb") as f:
         pickle.dump(cache, f)
         print(f"Successfully saved {cacheType.upper()} cache")
 
 
-def LoadCache(cacheType) -> dict | None:
-    if not CacheExists(cacheType):
+def load_cache(cacheType) -> dict | None:
+    if not does_cache_type_exist(cacheType):
         return None
-    with open(CacheFilePath(cacheType), "rb") as f:
+    with open(get_cache_filepath(cacheType), "rb") as f:
         cache = pickle.load(f)
         print(f"Successfully loaded {cacheType.upper()} cache")
     return cache
