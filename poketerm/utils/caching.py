50c41435059889b9f9e4342fe31df29238bc4e82
Dalton <daltonstwilliams@gmail.com>
2024-02-04 08:28:24 -0600

    Created caching.py

--- poketerm/utils/caching.py
+++ poketerm/utils/caching.py
@@ -3,6 +3,46 @@ import shutil
 import pickle
 
 
+class SubCache:
+    NAME_TO_ID = {}
+    ID_TO_DATA = {}
+
+
+class CacheManager:
+    # Will map some arbitrary string as a key
+    # CacheMappings["unique-key"] = SubCache
+    cache_mappings: dict[str, SubCache] = {}
+
+    @classmethod
+    def add_to_name_cache(cls, key, name, ID):
+        cls.cache_mappings.setdefault(key, SubCache()).NAME_TO_ID[name] = ID
+
+    @classmethod
+    def add_to_ID_cache(cls, key, ID, data):
+        cls.cache_mappings.setdefault(key, SubCache()).ID_TO_DATA[ID] = data
+
+    @classmethod
+    def get_ID_from_name(cls, key, name):
+        subcache = cls.cache_mappings.get(key, None)
+        if subcache:
+            return subcache.NAME_TO_ID.get(name)
+
+    @classmethod
+    def get_data_from_name(cls, key, name):
+        subcache = cls.cache_mappings.get(key, None)
+        if not subcache:
+            return None
+        ID = subcache.NAME_TO_ID.get(name, None)
+        if ID:
+            return subcache.ID_TO_DATA.get(ID, None)
+
+    @classmethod
+    def get_data_from_ID(cls, key, ID):
+        subcache = cls.cache_mappings.get(key, None)
+        if subcache:
+            return subcache.ID_TO_DATA.get(ID)
+
+
 def CacheDirectory():
     return os.path.join(os.path.expanduser("~"), os.sep, ".poketerm")
 
