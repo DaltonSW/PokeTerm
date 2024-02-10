bcbb642f5b3c5df7090a91ac51c1a65a3e3a7ce8
Dalton <daltonstwilliams@gmail.com>
2024-02-10 16:06:55 -0600

    Updated framework to load valid names (and other stuff but yknow)

--- poketerm/utils/api.py
+++ poketerm/utils/api.py
@@ -1,6 +1,7 @@
 import requests
 
 BASE_URL = "https://pokeapi.co/api/v2"
+LOCAL_URL = "http://localhost/api/v2"
 
 
 def get_ID_from_URL(URL: str) -> int:
@@ -8,10 +9,26 @@ def get_ID_from_URL(URL: str) -> int:
 
 
 def get_from_API(endpoint, searchTerm):
-    response = requests.get(f"{BASE_URL}/{endpoint}/{searchTerm}")
+    from poketerm.utils.testing import TEST_RUNNING
+
+    response = requests.get(
+        f"{LOCAL_URL if TEST_RUNNING else BASE_URL}/{endpoint}/{searchTerm}"
+    )
     if response.status_code == 404:
         return None
     return response.json()
+
+
+async def get_from_API_async(endpoint, searchTerm, session):
+    from poketerm.utils.testing import TEST_RUNNING
+
+    async with session.get(
+        f"{LOCAL_URL if TEST_RUNNING else BASE_URL}/{endpoint}/{searchTerm}"
+    ) as response:
+        resp = await response
+    if resp.status_code == 404:
+        return None
+    return resp.json()
 
 
 def get_from_URL(url) -> dict | None:
