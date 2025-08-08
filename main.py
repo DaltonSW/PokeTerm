fedebb0314fb150413b5bb45b64ac1961653168c
Dalton Williams <email@dalton.dog>
2025-08-08 15:14:25 -0500

    v2 starting nuke

--- main.py
+++
@@ -1,8 +0,0 @@
-from poketerm import main
-from poketerm.utils import testing
-import sys
-
-if __name__ == "__main__":
-    if len(sys.argv) > 1 and sys.argv[1] == "cache":
-        testing.handle_cache_test()
-    main.main()
--- poketerm/console.py
+++
@@ -1,4 +0,0 @@
-from rich.console import Console
-from poketerm.utils.colors import PokeTheme
-
-console = Console(theme=PokeTheme)
